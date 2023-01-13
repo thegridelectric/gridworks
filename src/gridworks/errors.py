822723945bb6887513f5318ad35546165b000f30
Jessica <jessica.lynn.millar@gmail.com>
2023-01-13 06:35:18 -0500

    Include registry error

--- src/gridworks/errors.py
+++ src/gridworks/errors.py
@@ -1,16 +1,18 @@
 class SchemaError(Exception):
     """Base class for Schema errors"""
-
     pass
 
 
 class AlgoError(Exception):
     """Base class for errors related to Algorand"""
-
     pass
 
 
 class DcError(Exception):
     """Base class for dataclass errors"""
+    pass
 
+
+class RegistryError(Exception)
+    """Base class for registry errors"""
     pass
