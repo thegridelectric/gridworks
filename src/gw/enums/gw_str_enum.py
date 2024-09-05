67c4bd1591ef1f493de1a07df25e44eedac048d1
Jessica Millar <jessica.lynn.millar@gmail.com>
2024-09-05 10:29:13 -0400

    better GwStrEnum

--- src/gw/enums/gw_str_enum.py
+++ src/gw/enums/gw_str_enum.py
@@ -1,24 +1,56 @@
-import enum
-from typing import Any
-from typing import Sequence
+from enum import StrEnum
+from typing import Any, List, Optional, Self
 
-
-class GwStrEnum(str, enum.Enum):
+class GwStrEnum(StrEnum):
     """
     Mimics fastapi-utils use of StrEnum, which diverges from the
-    python-native StrEnum for python 3.11+.  Specifically if
+    python-native StrEnum for python 3.11+.  Also, fills in with default
+    value if a string does not exist in the enum.
+    
+    Specifically (re difference with python StrEnum) if
 
     class Foo(GwStrEnum):
         Bar = auto()
 
     then
 
-    Foo.Bar.value is 'Bar'
+    Foo.Bar.value is 'Bar' (instead of 'bar')
 
     """
-
     @staticmethod
     def _generate_next_value_(
-        name: Any, start: int, count: int, last_values: Sequence[Any]
+        name: str,
+        start: int,  # noqa: ARG004
+        count: int,  # noqa: ARG004
+        last_values: list[Any],  # noqa: ARG004
     ) -> str:
-        return str(name)
+        return name
+
+    @classmethod
+    def values(cls) -> list[str]:
+        return [str(elt) for elt in cls]
+
+    @classmethod
+    def default(cls) -> Optional[Self]:
+        return None
+
+    @classmethod
+    def _missing_(cls, value: str) -> Self:
+        default = cls.default()
+        if default is None:
+            raise ValueError(f"'{value}' is not valid {cls.__name__}")
+        return default
+
+
+class SymbolizedEnum(GwStrEnum):
+    @classmethod
+    def symbol_to_value(cls, symbol: str) -> str:
+        raise NotImplementedError
+
+    @classmethod
+    def value_to_symbol(cls, value: str) -> str:
+        raise NotImplementedError
+    
+    @classmethod
+    def symbols(cls) -> List[str]:
+        raise NotImplementedError
