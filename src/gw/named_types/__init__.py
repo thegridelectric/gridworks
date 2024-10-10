c69250a5d3bcad6eb3f80cdccf4494506b198d44
Jessica Millar <jessica.lynn.millar@gmail.com>
2024-10-09 20:55:47 -0400

    Add named_type/gw_base

---
+++ src/gw/named_types/__init__.py
@@ -0,0 +1,6 @@
+from gw.named_types.gw_base import GwBase
+
+
+__all__ = [
+    GwBase
+]
---
+++ src/gw/named_types/gw_base.py
@@ -0,0 +1,54 @@
+import json
+from typing import Any, Dict, Literal, Type, TypeVar
+
+from gw.errors import GwTypeError
+from gw.utils import recursively_pascal, snake_to_pascal
+from pydantic import BaseModel, ConfigDict, ValidationError
+
+T = TypeVar("T", bound="GwBase")
+
+
+class GwBase(BaseModel):
+    type_name: Literal["gw.base"] = "gw.base"
+    version: Literal["000"] = "000"
+
+    model_config = ConfigDict(
+        alias_generator=snake_to_pascal,
+        frozen=True,
+        populate_by_name=True,
+        use_enum_values=True,
+    )
+
+    @classmethod
+    def from_type(cls, bytes) -> T:
+        try:
+            d = json.loads(bytes)
+        except TypeError as e:
+            raise GwTypeError("Type must be string or bytes!") from e
+        return cls.from_dict(d)
+
+    @classmethod
+    def from_dict(cls: Type[T], d: dict) -> T:
+        if not recursively_pascal(d):
+            raise GwTypeError(f"dict is not recursively pascal case! {d}")
+        try:
+            t = cls.model_validate(d)
+        except ValidationError as e:
+            raise GwTypeError(f"Pydantic validation error: {e}") from e
+        return t
+
+    def to_dict(self) -> Dict[str, Any]:
+        d = self.model_dump(exclude_none=True, by_alias=True)
+        return d
+
+    def to_type(self) -> bytes:
+        """
+        Serialize to the get.hourly.price.forecast.channel.000 representation designed to send in a message.
+        """
+        json_string = json.dumps(self.to_dict())
+        return json_string.encode("utf-8")
+
+    @classmethod
+    def type_name_value(cls) -> str:
+        # Automatically return the type_name defined in the subclass
+        return cls.model_fields["type_name"].default
