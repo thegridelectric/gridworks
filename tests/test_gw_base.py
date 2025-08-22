fff7dacde723a0722478300ec211640a1ef121bf
Jessica Millar <jessica.lynn.millar@gmail.com>
2025-08-22 08:16:23 -0500

    small eye roll

--- tests/test_gw_base.py
+++ tests/test_gw_base.py
@@ -2,5 +2,5 @@ from gw.named_types import GwBase
 
 
 def test_gw_base() -> None:
-    bytes = '{"TypeName":"gw.base","Version":"000"}'
+    bytes = '{"TypeName":"gw.base","Version":"000"}'.encode()
     assert GwBase.from_type(bytes).to_type() == bytes
