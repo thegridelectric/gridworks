a6efdb43ea58d29a81fc619faff9bc2624e602c7
Jessica Millar <jessica.lynn.millar@gmail.com>
2025-08-22 08:17:36 -0500

    fix pre-commit

--- tests/test_gw_base.py
+++ tests/test_gw_base.py
@@ -2,5 +2,5 @@ from gw.named_types import GwBase
 
 
 def test_gw_base() -> None:
-    bytes = '{"TypeName":"gw.base","Version":"000"}'.encode()
+    bytes = b'{"TypeName":"gw.base","Version":"000"}'
     assert GwBase.from_type(bytes).to_type() == bytes
